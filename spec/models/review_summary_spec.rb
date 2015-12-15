24df8862a011f0f89dc893d4b00225de9d6af4f5
Ben Forrest <ben@silvabox.com>
2015-12-15 11:00:17 +0000

    refactors challenge review test data into fixtures

--- spec/models/review_summary_spec.rb
+++ spec/models/review_summary_spec.rb
@@ -1,10 +1,14 @@
-describe ReviewSummary do 
+describe ReviewSummary do
+
+  review = YAML.load(File.read('./spec/fixtures/submit_challenge_review_data.yml'))
+  meta =  YAML.load(File.read('./spec/fixtures/submit_challenge_meta_data.yml'))
+  content = review.merge(meta)
+
   # headings in the real system is a lookup table with entries like so
   # 'appropriateuseofdependencyinjection' => 'Appropiate Use of Dependency Injection'
+  let(:headings) { Hash[content.keys.map{ |k| [k,k.upcase] }] }
 
-  let(:headings) { Hash[CONTENT.keys.map{ |k| [k,k.upcase] }] }
-
-  subject(:summary) { described_class.new(content: CONTENT, name: 'test_challenge', github_user: 'test_user', headings: headings) }
+  subject(:summary) { described_class.new(content: content, name: 'test_challenge', github_user: 'test_user', headings: headings) }
 
   it 'provides the name of the challenge' do
     expect(summary.name).to eq 'test_challenge'
@@ -15,7 +19,7 @@ describe ReviewSummary do
   end
 
   it 'lists the good parts' do
-    good_parts = REVIEW.values.reject { |content| content.empty? }
+    good_parts = review.values.reject { |content| content.empty? }
     # require 'byebug' ; byebug
     good_parts.each do |content|
       expect(summary.good_parts).to include content
@@ -23,7 +27,7 @@ describe ReviewSummary do
   end
 
   it 'does not include needs improvement in good parts' do
-    needs_improvement = REVIEW.select { |key, content| content.empty? }.map { |key, content| key.upcase }
+    needs_improvement = review.select { |key, content| content.empty? }.map { |key, content| key.upcase }
     needs_improvement.each do |content|
       expect(summary.good_parts).not_to include content
     end
@@ -32,14 +36,14 @@ describe ReviewSummary do
   it 'lists the parts needing improvement' do
     # the needs_improvement headings will come from the google doc.
     # the mock of this just returns the uppercase of the key to prove it is called.
-    needs_improvement = REVIEW.select { |key, content| content.empty? }.map { |key, content| key.upcase }
+    needs_improvement = review.select { |key, content| content.empty? }.map { |key, content| key.upcase }
     needs_improvement.each do |content|
       expect(summary.needs_improvement).to include content
     end
   end
 
   it 'does not include good parts in needing improvement' do
-    good_parts = REVIEW.values.reject { |content| content.empty? }
+    good_parts = review.values.reject { |content| content.empty? }
     good_parts.each do |content|
       expect(summary.needs_improvement).not_to include content.upcase
     end
@@ -48,33 +52,4 @@ describe ReviewSummary do
   it 'knows if there are additional comments' do
     expect(summary).to have_additional_comments
   end
-
-
-  REVIEW = {
-    'appropriateuseofdependencyinjection' => "The Twilio dependency can be injected appropriately",
-    'designforsingleresponsibilityprinciple' => "The design has at least `Takeaway` and `Order` classes or equivalent",
-    'ensurethatallgemsbeingusedareingemfile' => '',
-    'explicittestsforeveryelementofthepublicinterface' => "",
-    'explorethelanguageforsolutionstocommonproblems' => "The solution makes good use of the Ruby language, e.g., Hash.new(0) is used to simplify management of counts",
-    'lawofdemeter' => "The Law of Demeter is respected in the code base",
-    'openclosedprinciple' => "The open closed principle has been respected, e.g. food items are not hard coded into restaurant",
-    'personaldetailsandtokensongithub' => "ENV vars have been used in place of sensitive data",
-    'separationofconcerns' => "Business and presentation logic has been cleanly separated",
-    'stubbingthetwilioapi' => "The Twilio API is stubbed appropriately",
-    'testsshouldtestrealbehavioursnotstubs' => "All tests test real behaviour and not stubs",
-    'useconsistentstylesandindentation' => "The code follows the Ruby style and coding conventions",
-  }
-
-  META =  {
-    'timestamp' => "10/23/2015 16:07:09",
-    'whatistherevieweesgithubusername' => "sarahkristinepedersen",
-    'whosechallengeareyoureviewing' => "Sara",
-    'yourname' => "Jongmin Kim",
-    'didyoufindthisformusefulincompletingthereview' => "I find the review introduction page very useful and helpful. If juniors have access to this information after each challenge, they'd give a better code review when they're seniors.",
-    'adddetailsofyouralternateapproachtothereviewifyouskippedtherest' => '',
-    'anyadditionalcommentsonthecodeyoureviewed' => 'testing additional comments'
-  }
-
-  CONTENT = REVIEW.merge(META)
-
 end
--- spec/services/submit_challenge_review_spec.rb
+++ spec/services/submit_challenge_review_spec.rb
@@ -1,9 +1,15 @@
 describe SubmitChallengeReview do
-  subject(:service) { SubmitChallengeReview.new(content: CONTENT, name: 'test_challenge', github_user: 'test_user') }
+
+  review = YAML.load(File.read('./spec/fixtures/submit_challenge_review_data.yml'))
+  meta =  YAML.load(File.read('./spec/fixtures/submit_challenge_meta_data.yml'))
+  content = review.merge(meta)
+
+
+  subject(:service) { SubmitChallengeReview.new(content: content, name: 'test_challenge', github_user: 'test_user') }
 
   let(:session) { double(:session, spreadsheet_by_key: document) }
   let(:document) { double(:document, worksheet_by_gid: worksheet) }
-  let(:worksheet) { double(:document, rows: [CONTENT.keys.map(&:to_s).map(&:upcase)]) }
+  let(:worksheet) { double(:document, rows: [content.keys.map(&:to_s).map(&:upcase)]) }
 
   let(:github_client) { double(:github_client, pull_requests: [pull_request], add_comment: nil) }
   let(:pull_request) { double(:pull_request, number: 1234) }
@@ -20,19 +26,19 @@ describe SubmitChallengeReview do
   describe '::with' do
     it 'passes arguments to new instance' do
       expect(SubmitChallengeReview).to receive(:new) do |options|
-        expect(options[:content]).to eq CONTENT
+        expect(options[:content]).to eq content
         expect(options[:name]).to eq 'test_challenge'
         expect(options[:github_user]).to eq 'test_user'
       end.and_call_original
 
       allow_any_instance_of(SubmitChallengeReview).to receive(:run)
 
-      SubmitChallengeReview.with(content: CONTENT, name: 'test_challenge', github_user: 'test_user')
+      SubmitChallengeReview.with(content: content, name: 'test_challenge', github_user: 'test_user')
     end
 
     it 'runs an instance' do
       expect_any_instance_of(SubmitChallengeReview).to receive(:run)
-      SubmitChallengeReview.with(content: CONTENT, name: 'test_challenge', github_user: 'test_user')
+      SubmitChallengeReview.with(content: content, name: 'test_challenge', github_user: 'test_user')
     end
   end
 
@@ -48,32 +54,4 @@ describe SubmitChallengeReview do
     end
     service.run
   end
-
-
-  REVIEW = {
-    'appropriateuseofdependencyinjection' => "The Twilio dependency can be injected appropriately",
-    'designforsingleresponsibilityprinciple' => "The design has at least `Takeaway` and `Order` classes or equivalent",
-    'ensurethatallgemsbeingusedareingemfile' => '',
-    'explicittestsforeveryelementofthepublicinterface' => "",
-    'explorethelanguageforsolutionstocommonproblems' => "The solution makes good use of the Ruby language, e.g., Hash.new(0) is used to simplify management of counts",
-    'lawofdemeter' => "The Law of Demeter is respected in the code base",
-    'openclosedprinciple' => "The open closed principle has been respected, e.g. food items are not hard coded into restaurant",
-    'personaldetailsandtokensongithub' => "ENV vars have been used in place of sensitive data",
-    'separationofconcerns' => "Business and presentation logic has been cleanly separated",
-    'stubbingthetwilioapi' => "The Twilio API is stubbed appropriately",
-    'testsshouldtestrealbehavioursnotstubs' => "All tests test real behaviour and not stubs",
-    'useconsistentstylesandindentation' => "The code follows the Ruby style and coding conventions",
-  }
-
-  META =  {
-    'timestamp' => "10/23/2015 16:07:09",
-    'whatistherevieweesgithubusername' => "sarahkristinepedersen",
-    'whosechallengeareyoureviewing' => "Sara",
-    'yourname' => "Jongmin Kim",
-    'didyoufindthisformusefulincompletingthereview' => "I find the review introduction page very useful and helpful. If juniors have access to this information after each challenge, they'd give a better code review when they're seniors.",
-    'adddetailsofyouralternateapproachtothereviewifyouskippedtherest' => '',
-    'anyadditionalcommentsonthecodeyoureviewed' => 'testing additional comments'
-  }
-
-  CONTENT = REVIEW.merge(META)
 end

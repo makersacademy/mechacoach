7059281c95bc11b45686e04d72dbbb6230e07e41
Ben Forrest <ben@silvabox.com>
2016-04-11 15:41:01 +0100

    changes channel existence validation logic

--- lib/find_channel.rb
+++ lib/find_channel.rb
@@ -4,7 +4,7 @@ class FindChannel
   def self.with(channel)
     notifier = SlackNotifier.new({team: 'makersstudents', channel: slackified_channel(channel)})
     # If the channel does not exist, the error message is 'Invalid channel specified'
-    notifier.notify('') == "No text specified"
+    notifier.notify('') != "Invalid channel specified"
   end
 
   private
